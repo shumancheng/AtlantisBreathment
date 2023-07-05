92e2b2e4a3d0a9e05a2fce43eb3bdac3394f26d4
Alexi <alexis0@web.de>
2023-07-05 04:18:54 +0200

    ProgressBarOfficial3

--- ProgressBarr/Assets/Scripts/ProgressBar.cs
+++ ProgressBarr/Assets/Scripts/ProgressBar.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     private Slider slider;
+    private ParticleSystem particleSys;
+
 
     public float FillSpeed = 0.5f;
     private float targetProgress = 0;
@@ -13,18 +15,28 @@ public class ProgressBar : MonoBehaviour
     private void Awake()
     {
         slider = gameObject.GetComponent<Slider>();
+        particleSys = GameObject.Find("ProgressBarParticleSystem").GetComponent<ParticleSystem>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        IncrementProgress(1f);
+       IncrementProgress(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (slider.value < targetProgress)
+        {
             slider.value += FillSpeed * Time.deltaTime;
+            if (!particleSys.isPlaying)
+                particleSys.Play();
+        }
+        else
+        {
+            particleSys.Stop();
+        }
+            
     }
 
     public void IncrementProgress(float newProgress)
